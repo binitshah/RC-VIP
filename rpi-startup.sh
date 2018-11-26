1802197a719398b892946190e57ad1df5d6405cb
rc-cip-rpi3 <rc-vip-rpi3@example.com>
2018-11-26 18:56:09 +0000

    adds rpi_startup launch file

--- rpi-startup.sh
+++ rpi-startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source /home/ubuntu/catkin_ws/install/setup.bash
+source /home/ubuntu/catkin_ws/devel/setup.bash
 
-rosrun rosserial_python serial_node.py _port:=/dev/ttyAMA0 _baud:=57600 &
-roslaunch raspicam_node camerav1_1280x720.launch &
+# rosrun rosserial_python serial_node.py _port:=/dev/ttyAMA0 _baud:=57600 &
+# roslaunch raspicam_node camerav1_1280x720.launch &
+
+roslaunch rc_vip rpi_startup.launch
+
